57a64401100abf21922fac9b8602f921a3e5bfdd
Quentin <quentin.larose@etu.unice.fr>
2020-04-25 03:40:05 +0200

    Avancement fragment map
    Avancement fragment map

---
+++ Project/app/src/main/java/com/example/freshprox/search/IButtonClickListener.java
@@ -0,0 +1,7 @@
+package com.example.freshprox.search;
+
+import android.view.View;
+
+public interface IButtonClickListener {
+    void onButtonMapClicked(View button);
+}
--- Project/app/src/main/java/com/example/freshprox/search/SearchActivity.java
+++ Project/app/src/main/java/com/example/freshprox/search/SearchActivity.java
@@ -1,17 +1,32 @@
 package com.example.freshprox.search;
 
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.freshprox.R;
+import com.example.freshprox.main.MainFragment;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity implements View.OnClickListener{
+    private IButtonClickListener mCallBack;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.search_activity);
+        setContentView(R.layout.activity_search);
+        MainFragment mainFragment = (MainFragment) getSupportFragmentManager().findFragmentById(R.id.frame_layout_map);
+        if(mainFragment == null){
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.frame_layout_map, new MapFragment());
+            transaction.addToBackStack(null);
+            transaction.commit();
+        }
+    }
+
+    @Override
+    public void onClick(View button) {
+        if(button.getId() == R.id.activity_main_button_map)mCallBack.onButtonMapClicked(button);
     }
 }
