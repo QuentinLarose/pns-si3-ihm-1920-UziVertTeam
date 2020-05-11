265f3e7f1414c37b74530ba89b23ebef8ecc233f
Quentin <quentin.larose@etu.unice.fr>
2020-05-11 03:52:06 +0200

    Ajout bouton "Fermer Map" + update fonctionne Map
    Ajout bouton "Fermer Map" + update fonctionne Map

--- Project/app/src/main/java/com/example/freshprox/search/SearchActivity.java
+++ Project/app/src/main/java/com/example/freshprox/search/SearchActivity.java
@@ -2,13 +2,12 @@ package com.example.freshprox.search;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.example.freshprox.R;
-import com.example.freshprox.main.MainFragment;
 
 public class SearchActivity extends AppCompatActivity implements View.OnClickListener, IButtonClickListener{
     private IButtonClickListener mCallBack;
@@ -18,34 +17,37 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.frame_layout_map);
-        if(mapFragment == null){
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-            transaction.replace(R.id.frame_layout_map, new MapFragment());
-            transaction.addToBackStack(null);
-            transaction.commit();
-        }
-        /*
-        MainFragment mainFragment = (MainFragment) getSupportFragmentManager().findFragmentById(R.id.frame_layout_map);
-        if(mainFragment == null){
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-            transaction.replace(R.id.frame_layout_map, new MapFragment());
-            transaction.addToBackStack(null);
-            transaction.commit();
-        }
-         */
+        Toast.makeText(this,"dans le fragment dessous avec: ", Toast.LENGTH_LONG).show();
+        findViewById( R.id.search_activity_button_map).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onButtonMapClicked(v);
+            }
+        });
+        findViewById(R.id.closeMapButton).setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mapFragment != null){
+                    Toast.makeText(v.getContext(),"Suppression Fragment: ", Toast.LENGTH_LONG).show();
+                    getSupportFragmentManager().beginTransaction().remove(mapFragment).commit();
+                    mapFragment = null;
+                }
+            }
+        });
     }
 
     @Override
     public void onClick(View button) {
-        if(button.getId() == R.id.activity_main_button_map)mCallBack.onButtonMapClicked(button);
+        Toast.makeText(this,"dans le fragment dessous avec: ", Toast.LENGTH_LONG).show();
+        if(button.getId() == R.id.search_activity_button_map)mCallBack.onButtonMapClicked(button);
+        Toast.makeText(this,"ZOUZA: ", Toast.LENGTH_LONG).show();
     }
 
 
 
     @Override
     public void onButtonMapClicked(View button) {
-        /*
+
         Toast.makeText(this,"dans le fragment dessous avec: ", Toast.LENGTH_LONG).show();
        mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.frame_layout_map);
         if(mapFragment == null){
@@ -54,8 +56,6 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.frame_layout_map,mapFragment).commit();
         }
-
-         */
     }
 
 
