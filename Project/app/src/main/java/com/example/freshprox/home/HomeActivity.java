e5c2f2aa586637b19b0548d5491c953a1fd86bac
Quentin <quentin.larose@etu.unice.fr>
2020-05-11 16:54:42 +0200

    Ajout HomeActivity et xml
    Ajout HomeActivity et xml

--- Project/app/src/main/java/com/example/freshprox/home/HomeActivity.java
+++ Project/app/src/main/java/com/example/freshprox/home/HomeActivity.java
@@ -1,8 +1,25 @@
 package com.example.freshprox.home;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.freshprox.R;
+import com.example.freshprox.main.MainFragment;
 
 public class HomeActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+        Log.d("TAG","MainActivity");
+    }
 
 }
