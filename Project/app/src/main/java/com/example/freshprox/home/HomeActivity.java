da08251e91cf7facc39c24f455060b414a29243f
Quentin <quentin.larose@etu.unice.fr>
2020-05-11 17:35:32 +0200

    Mise à jour HomeActivity

--- Project/app/src/main/java/com/example/freshprox/home/HomeActivity.java
+++ Project/app/src/main/java/com/example/freshprox/home/HomeActivity.java
@@ -2,8 +2,12 @@ package com.example.freshprox.home;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 
 import com.example.freshprox.R;
 import com.example.freshprox.main.MainFragment;
@@ -13,13 +17,20 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.act);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow();
-        }
-        Log.d("TAG","MainActivity");
+        setContentView(R.layout.activity_home);
+        findViewById( R.id.google_connection_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        findViewById(R.id.guest_connection_button).setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(),"Suppression Fragment: ", Toast.LENGTH_LONG).show();
+            }
+        });
+        Log.d("TAG","HomeActivity");
     }
 
 }
