25c74211881e669cfcaaeaa7088cc62d6a1ddf89
Quentin <quentin.larose@etu.unice.fr>
2020-05-11 02:58:44 +0200

    Ajout du fragment contenant la Map des Producteurs
    Ajout du fragment contenant la Map des Producteurs

--- Project/app/src/main/java/com/example/freshprox/search/MapFragment.java
+++ Project/app/src/main/java/com/example/freshprox/search/MapFragment.java
@@ -1,6 +1,76 @@
 package com.example.freshprox.search;
 
-import androidx.fragment.app.Fragment;
+import android.Manifest;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
-public class MapFragment extends Fragment {
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceManager;
+
+import com.example.freshprox.BuildConfig;
+import com.example.freshprox.R;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
+import org.osmdroid.views.overlay.OverlayItem;
+
+import java.util.ArrayList;
+
+public class MapFragment extends Fragment implements View.OnClickListener{
+    private MapView map;
+    IMapController mapController;
+
+    public MapFragment(){}
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View v = inflater.inflate(R.layout.map_fragment, container, false);
+        map = (MapView) v.findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        configMap();
+        return v;
+    }
+
+    private void configMap(){
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+
+
+        map.setFlingEnabled(true);
+        IMapController mapController = map.getController();
+        mapController.setZoom(6);
+        GeoPoint startPoint = new GeoPoint(50.77623, 6.06937);
+        mapController.setCenter(startPoint);
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        map.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        map.onResume();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
--- Project/app/src/main/java/com/example/freshprox/search/SearchActivity.java
+++ Project/app/src/main/java/com/example/freshprox/search/SearchActivity.java
@@ -2,6 +2,7 @@ package com.example.freshprox.search;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
@@ -9,13 +10,22 @@ import androidx.fragment.app.FragmentTransaction;
 import com.example.freshprox.R;
 import com.example.freshprox.main.MainFragment;
 
-public class SearchActivity extends AppCompatActivity implements View.OnClickListener{
+public class SearchActivity extends AppCompatActivity implements View.OnClickListener, IButtonClickListener{
     private IButtonClickListener mCallBack;
+    private MapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.frame_layout_map);
+        if(mapFragment == null){
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.frame_layout_map, new MapFragment());
+            transaction.addToBackStack(null);
+            transaction.commit();
+        }
+        /*
         MainFragment mainFragment = (MainFragment) getSupportFragmentManager().findFragmentById(R.id.frame_layout_map);
         if(mainFragment == null){
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
@@ -23,10 +33,30 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
             transaction.addToBackStack(null);
             transaction.commit();
         }
+         */
     }
 
     @Override
     public void onClick(View button) {
         if(button.getId() == R.id.activity_main_button_map)mCallBack.onButtonMapClicked(button);
     }
+
+
+
+    @Override
+    public void onButtonMapClicked(View button) {
+        /*
+        Toast.makeText(this,"dans le fragment dessous avec: ", Toast.LENGTH_LONG).show();
+       mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.frame_layout_map);
+        if(mapFragment == null){
+            mapFragment = new MapFragment();
+            Bundle args = new Bundle();
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.frame_layout_map,mapFragment).commit();
+        }
+
+         */
+    }
+
+
 }
