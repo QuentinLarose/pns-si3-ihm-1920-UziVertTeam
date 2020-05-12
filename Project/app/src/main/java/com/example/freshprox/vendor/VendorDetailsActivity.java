e8b0b1711a576ff0fa6be48ed6fab240bb19267f
Timothee Poulain <timothee.poulain@etu.univ-cotedazur.fr>
2020-05-12 21:44:09 +0200

    Correction problème détails du vendeur

--- Project/app/src/main/java/com/example/freshprox/vendor/VendorDetailsActivity.java
+++ Project/app/src/main/java/com/example/freshprox/vendor/VendorDetailsActivity.java
@@ -1,5 +1,6 @@
 package com.example.freshprox.vendor;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -9,6 +10,11 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.freshprox.R;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class VendorDetailsActivity extends AppCompatActivity {
 
@@ -18,7 +24,18 @@ public class VendorDetailsActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vendor_layout);
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("vendors list", null);
+        Type type = new TypeToken<ArrayList<Vendor>>() {}.getType();
+        ArrayList<Vendor> vendorsList = gson.fromJson(json, type);
+
+        if (vendorsList == null){
+            vendorsList = new ArrayList<>();
+        }
+
         this.vendors = new ListOfVendor(false);
+        vendors.addAll(vendorsList);
         Vendor vendor = vendors.get(getIntent().getIntExtra("position",0));
         RelativeLayout vendorDesc = findViewById(R.id.vend_desc);
         TextView name = vendorDesc.findViewById(R.id.name);
